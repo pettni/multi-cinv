81b4a520c309b0ab149192a759bb2b5db64d84c4
Stanley Smith <swsmth@umich.edu>
2016-03-14 14:05:37 -0400

    Updated models and controller for paper.

--- simulation/constants.m
+++ simulation/constants.m
@@ -1,6 +1,6 @@
 function con = constants
 
-	% Common parameters
+    % Common parameters
     con.m = 1650 ; %kg
 
     % ACC parameters
@@ -13,5 +13,25 @@ function con = constants
     con.b = 1.59; % m
 	con.Caf = 1.33e5; % N/rad 
     con.Car = 9.88e4; % N/rad
-
+    
+    % sampling time
+    con.dt = 0.1;
+    
+    A =   [ 1.0000         0;
+           -1.0000         0;
+                 0   -1.0000;
+            0.1960   -0.9806;
+            0.0995   -0.9950;
+            0.2870   -0.9579;
+            0.3708   -0.9287;
+            0.4464   -0.8948;
+            0.5134   -0.8581;
+            0.5721   -0.8202];
+    
+  b = [];
+  
+  con.acc_data = Polyhedron(A,b);
+  
+  con.lk_data = load('lk_invariant');
+  
 end
--- simulation/lk_controller.m
+++ simulation/lk_controller.m
@@ -10,7 +10,7 @@ function setup(block)
   block.NumDialogPrms = 0;
 
   %% Register number of input and output ports
-  block.NumInputPorts  = 1;
+  block.NumInputPorts  = 2;
   block.NumOutputPorts = 1;
 
   %% Setup functional port properties to dynamically
@@ -20,6 +20,9 @@ function setup(block)
  
   block.InputPort(1).Dimensions        = 4;
   block.InputPort(1).DirectFeedthrough = false;
+  
+  block.InputPort(2).Dimensions        = 2;
+  block.InputPort(2).DirectFeedthrough = false;
 
   block.OutputPort(1).Dimensions       = 1;
   
@@ -37,12 +40,57 @@ function setup(block)
 
 function InitConditions(block)
 
-
 %endfunction
 
 function Output(block)
+  global con
+  
   x_lk = block.InputPort(1).Data;
-  block.OutputPort(1).Data = -0.1*(x_lk(1));
+  x_acc = block.InputPort(2).Data;
+  
+  % get speed from acc state
+  v = x_acc(1);
+  
+  if(v == 0)
+      v = 27;
+  end
+  
+  A = [0 1 v 0;
+    0 -(con.Caf+con.Car)/(con.m*v) 0 ((con.b*con.Car-con.a*con.Caf)/(con.m*v) - v);
+    0 0 0 1;
+    0 (con.b*con.Car-con.a*con.Caf)/(con.Iz*v)  0 -(con.a^2 * con.Caf + con.b^2 * con.Car)/(con.Iz*v)];
+  B = [0;con.Caf/con.m; 0; con.a*con.Caf/con.Iz];
+  E = [0;0;1;0];
+  K = [0;0;0;0];
+  
+  A = A*con.dt + eye(4);
+  B = B*con.dt;
+  E = E*con.dt;
+  K = K*con.dt;
+  
+  % assume no disturbance
+  XD_plus = 0;
+  XD_minus = 0;
+  
+  R_x = eye(4);
+  r_x = [1; 1; 1; 1;];
+  R_u = 1;
+  r_u = 1;
 
+  H_x = con.lk_data.C.A;
+  h_x = con.lk_data.C.b;
+
+  H = B'*R_x*B + R_u;
+  f = r_u + B'*R_x*(A*x_lk + K) + B'*r_x;
+  A_quad = [H_x*B; H_x*B];
+  b_quad = [h_x - H_x*A*x_lk - H_x*E*XD_plus; h_x - H_x*A*x_lk - H_x*E*XD_minus];
+
+  u = quadprog(H, f, A_quad, b_quad);
+  
+  if(size(u) == 0)
+      u = -0.1*(x_lk(1));
+  end
+  
+  block.OutputPort(1).Data = u;
+  
 %endfunction
-
